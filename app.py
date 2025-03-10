ce3dfd797e646167afa6d9ffa6f3aba6de36faf5
younes_qasempour <qasempouryounes@gmail.com>
2025-03-10 17:04:47 +0330

    Initial commit

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    response = requests.get('https://api.npoint.io/c790b4d5cab58020d391')
+    posts = response.json()
+    return render_template("index.html", posts=posts)
+
+
+@app.route('/post/<int:blog_id>')
+def get_post(blog_id):
+    response = requests.get('https://api.npoint.io/c790b4d5cab58020d391')
+    post = response.json()[blog_id - 1]
+    return render_template("post.html", post=post)
 
 
 if __name__ == '__main__':
